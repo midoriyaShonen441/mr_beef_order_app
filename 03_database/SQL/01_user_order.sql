2aa7e065a8807a03d01289fde03e7f55d02f0240
“midoriyaShonen441” <khunnatta@gmail.com>
2022-08-08 17:20:56 +0700

    feat: finish user_order & menu_list table script

--- 03_database/SQL/01_user_order.sql
+++ 03_database/SQL/01_user_order.sql
@@ -1,6 +1,8 @@
 CREATE TABLE user_order (
     order_id VARCHAR(30) NOT NULL ,
     table_no INT NOT NULL,
-    menu_id
+    menu_id INT NOT NULL,
+    qty INT,
+    status VARCHAR(30),
     PRIMARY KEY (order_id)
 )
