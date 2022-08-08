31eff94772247a675c29d0d4d958ee339fbdf86c
mikeGLA <mikecommand_conquer@hotmail.com>
2022-08-08 17:05:37 +0700

    push update loginpage and dashboard

---
+++ 01_frontend/src/frontend-kitchen/order-Menu.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="customer-Menu-Index">
+        <div class="Table-No">dsafdsa</div>
+        <div class="Menu-ID">sadfdsa</div>
+        <div class="Menu-Name">sdafd</div>
+        <div class="Menu-Count">fdsaf</div>
+        <div class="Confirm-Button">sdafds</div>
+        <div class="Clear-Button">dsafad</div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .customer-Menu-Index{
+        background: white;
+        height: 100px;
+        border: 1px solid orange;
+        display: flex;
+        justify-content: space-between;
+        margin: auto;
+    }
+
+</style>
